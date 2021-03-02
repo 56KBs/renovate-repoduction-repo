8d0b183c603f1784d17d42ea3d518c7c7142ac49
Matthew Palmer <matthew.palmer@helpsystems.com>
2021-03-02 15:17:05 +0000

    Introduce more submodules & nested submodules

---
+++ c/go.mod
@@ -0,0 +1,5 @@
+module github.com/56KBs/renovate-reproduction-repo/c
+
+go 1.16
+
+require github.com/caarlos0/env/v5 v5.1.4
---
+++ c/v2/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+
+	env "github.com/caarlos0/env/v5"
+)
+
+type environment struct {
+	Home string `env:"HOME"`
+}
+
+func main() {
+	environment := environment{}
+	if err := env.Parse(&environment); err != nil {
+		fmt.Printf("%+v\n", err)
+	}
+
+	fmt.Printf("%+v\n", environment)
+}
